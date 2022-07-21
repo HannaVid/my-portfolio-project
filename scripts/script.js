0f81973b44d5113669abbcdf97f0a166fc24f90b
HannaVid <hannaviden@gmail.com>
2022-07-21 17:01:45 +0300

    Added glowing dotted background for hero

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,46 @@
 // Menu
 
-let menuBurger = document.querySelector(".header_menu");
-let headerBurger = document.querySelector(".header_burger");
-let body = document.querySelector("body");
+// let menuBurger = document.querySelector(".header_menu");
+// let headerBurger = document.querySelector(".header_burger");
+// let body = document.querySelector("body");
 
-headerBurger.addEventListener(`click`, function () {
-  let toggleClassMenu = menuBurger.classList.toggle("active");
-  let toggleClassHeader = headerBurger.classList.toggle("active");
-  let toggleClassBody = body.classList.toggle("lock");
-});
+// headerBurger.addEventListener(`click`, function () {
+//   let toggleClassMenu = menuBurger.classList.toggle("active");
+//   let toggleClassHeader = headerBurger.classList.toggle("active");
+//   let toggleClassBody = body.classList.toggle("lock");
+// });
+
+// Create dotted background
+
+// let windowHeight = window.innerHeight;
+// console.log(windowHeight);
+
+// let windowWidth = window.innerWidth;
+// console.log(windowWidth);
+
+window.addEventListener("DOMContentLoaded", createDottedBackground);
+
+function createDottedBackground() {
+  function createDots() {
+    let canvas = document.getElementById(`canvas`);
+    let width = canvas.offsetWidth;
+    let height = canvas.offsetHeight;
+
+    console.log(width);
+    console.log(height);
+
+    for (let i = 0; i < width; i += 40) {
+      for (let j = 0; j < height; j += 40) {
+        let outerDiv = document.createElement("div");
+        outerDiv.classList.add("box");
+        //   console.log(outerDiv);
+        let innerDiv = document.createElement("div");
+        innerDiv.classList.add("dot");
+        outerDiv.appendChild(innerDiv);
+        canvas.appendChild(outerDiv);
+      }
+    }
+  }
+  createDots();
+  window.addEventListener("resize", createDots);
+}
