1aa08e72f7b0a2175a394efe27f1aac07a1eb25f
HannaVid <hannaviden@gmail.com>
2022-07-22 23:03:37 +0300

    Added a footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,22 @@
 // Menu
 
-// let menuBurger = document.querySelector(".header_menu");
-// let headerBurger = document.querySelector(".header_burger");
-// let body = document.querySelector("body");
+let menuBurger = document.querySelector(".header_menu");
+let headerBurger = document.querySelector(".header_burger");
+let body = document.querySelector("body");
 
-// headerBurger.addEventListener(`click`, function () {
-//   let toggleClassMenu = menuBurger.classList.toggle("active");
-//   let toggleClassHeader = headerBurger.classList.toggle("active");
-//   let toggleClassBody = body.classList.toggle("lock");
-// });
+headerBurger.addEventListener(`click`, function () {
+  let toggleClassMenu = menuBurger.classList.toggle("active");
+  let toggleClassHeader = headerBurger.classList.toggle("active");
+  let toggleClassBody = body.classList.toggle("lock");
+});
 
 // Create dotted background
 
-// let windowHeight = window.innerHeight;
-// console.log(windowHeight);
+let windowHeight = window.innerHeight;
+console.log(windowHeight);
 
-// let windowWidth = window.innerWidth;
-// console.log(windowWidth);
+let windowWidth = window.innerWidth;
+console.log(windowWidth);
 
 window.addEventListener("DOMContentLoaded", createDottedBackground);
 
